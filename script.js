ca71455e3821933c4f4948caa961fc99e4f72e60
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-13 20:11:51 -0300

    UPDATE: requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const send = document.getElementById('send');
+const checkbox = document.getElementById('agreement');
+const submit = document.getElementById('submit-btn');
 
 function verify() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -10,3 +12,13 @@ function verify() {
   }
 }
 send.addEventListener('click', verify);
+
+function checkAgree() {
+  if (checkbox.checked !== true) {
+    submit.setAttribute('disabled', true);
+  } else {
+    submit.removeAttribute('disabled');
+  }
+}
+
+checkbox.addEventListener('click', checkAgree);
