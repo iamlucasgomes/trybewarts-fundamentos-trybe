a3d07b608acda1f504719ff726bff8973b8ec48c
Antonio Cunha <antonio.frango@gmail.com>
2022-07-13 16:46:41 -0300

    UPDATE: requisito 03

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const send = document.getElementById('send');
 
 function verify() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
-    alert('Olá, Tryber!');
-  }
+  alert('Olá, Tryber!');
+  } else {
+  alert('Email ou senha inválidos.');
+} 
 }
 send.addEventListener('click', verify);
