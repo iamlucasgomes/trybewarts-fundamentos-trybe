a4f3f9df81d92088f77143b24cd78e689835b8a4
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-13 16:39:25 -0300

    UPDATE: requisito 03

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const email = document.getElementById(email)
-const password = document.getElementById(password)
-const send = document.getElementById(send)
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const send = document.getElementById('send');
 
-function verify (){
-    if (email.value === "tryber@teste.com" && password.value === "123456"){
-        alert("Olá, Tryber!")
-    }
-} 
-send.addEventListener("click", verify) 
+function verify() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  }
+}
+send.addEventListener('click', verify);
