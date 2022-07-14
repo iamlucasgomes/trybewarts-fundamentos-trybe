89d08513ed403aa2fc0d18c4206a1b12a31acca1
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-13 23:58:58 -0300

    UPDATE: requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const password = document.getElementById('password');
 const send = document.getElementById('send');
 const checkbox = document.getElementById('agreement');
 const submit = document.getElementById('submit-btn');
+const textArea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 function verify() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,7 +13,6 @@ function verify() {
     alert('Email ou senha inválidos.');
   }
 }
-send.addEventListener('click', verify);
 
 function checkAgree() {
   if (checkbox.checked !== true) {
@@ -20,5 +21,11 @@ function checkAgree() {
     submit.removeAttribute('disabled');
   }
 }
+function maxLengthText() {
+  const lengthText = textArea.maxLength - textArea.textLength;
+  counter.innerHTML = `Você ainda pode escrever ${lengthText} caracteres`;
+}
 
+textArea.addEventListener('keyup', maxLengthText);
+send.addEventListener('click', verify);
 checkbox.addEventListener('click', checkAgree);
